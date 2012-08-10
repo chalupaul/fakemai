96adb074006acc4a9fa96f2803cc7cef99062e99
chalupaul <paul@paulsresume.net>
2012-08-09 23:48:20 -0500

    Tossed in a few changes that made genurl automagically populate its links better.

--- genurl.php
+++ genurl.php
@@ -2,10 +2,10 @@
 <?
 require_once('config.php');
 require_once('URLToken.php');
+$aGenRequestPath = dirname($_SERVER['REQUEST_URI']);
 
-print "<p><a href=\"http://$_SERVER[SERVER_NAME]" . urlauth_gen_url($sURL, $sParam, $nWindow, $sSalt, $sExtract, time()) . "\">good link, generated with time()</a></p>\n";
-print "<p><a href=\"http://$_SERVER[SERVER_NAME]" . urlauth_gen_url($sURL, $sParam, $nWindow, $sSalt, '192.168.10.10', time()) . "\">bad link, different ip address</a></p>\n";
-print "<p><a href=\"http://$_SERVER[SERVER_NAME]" . urlauth_gen_url($sURL, $sParam, $nWindow, $sSalt, $sExtract, time() - 172800) . "\">old link, generated 2 days ago</a></p>\n";
-
+print "<p><a href=\"http://$_SERVER[SERVER_NAME]$aGenRequestPath" . urlauth_gen_url($sURL, $sParam, $nWindow, $sSalt, $sExtract, time()) . "\">good link, generated with time()</a></p>\n";
+print "<p><a href=\"http://$_SERVER[SERVER_NAME]$aGenRequestPath" . urlauth_gen_url($sURL, $sParam, $nWindow, $sSalt, '192.168.10.10', time()) . "\">bad link, different ip address</a></p>\n";
+print "<p><a href=\"http://$_SERVER[SERVER_NAME]$aGenRequestPath" . urlauth_gen_url($sURL, $sParam, $nWindow, $sSalt, $sExtract, time() - 172800) . "\">old link, generated 2 days ago</a></p>\n";
 ?>
 </body></html>
